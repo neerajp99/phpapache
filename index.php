936a08fae94b1de6af48f57d4a1456d1b5f3e9dc
YOUR NAME <neerajp1999@gmail.com>
2015-12-29 19:09:21 +0530

    Added all details

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Whomi</title>
+</head>
+<body>
+	<form action="whomi.php" method="POST">
+		<input type="text" name="name" placeholder="Enter your name">
+		<input type="submit" name="submit">
+
+	</form>
+
+
+</body>
+</html>
---
+++ whoami.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Whomi</title>
+</head>
+<body>
+
+<?php 
+	
+	if (isset($_POST['name'])){
+		echo "Hello, ".$_POST['name'];
+	}
+?>
+
+
+</body>
+</html>
