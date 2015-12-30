fa852e6eddf3d136b4aa2db2489cbd43d438816f
YOUR NAME <neerajp1999@gmail.com>
2015-12-30 11:38:56 +0530

    final code

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
+<!DOCTYPE html>
 <html>
+
 <head>
-	<title>Whomi</title>
+    <meta charset="UTF-8">
+    <title>Flat Login Form</title>
+    <link rel="stylesheet prefetch" href="http://daneden.github.io/animate.css/animate.min.css">
+    <link rel="stylesheet prefetch" href="http://fonts.googleapis.com/css?family=Roboto:400,100,400italic,700italic,700">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Chelsea+Market" rel="stylesheet" type="text/css">
+    <link rel="stylesheet prefetch" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
+
 <body>
-	<form action="whomi.php" method="POST">
-		<input type="text" name="name" placeholder="Enter your name">
-		<input type="submit" name="submit">
+    <h1 class="center">Hello, [your name]!</h1>
+    <div class="form animated bounceIn">
+        <h2>Enter your name</h2>
+        <form action="whoami.php" method="POST">
+            <input placeholder="Your name" type="text" name="name">
+            <button class="animated infinite pulse" type="submit">Submit</button>
+        </form>
+    </div>
 
-	</form>
-
-
+    <footer>
+        <h4>Made with &nbsp;<i class="fa fa-heart"></i>&nbsp;for Fossasia.org</footer>
 </body>
+
+
 </html>
+
+
--- whoami.php
+++ whoami.php
@@ -1,16 +1,27 @@
+<!DOCTYPE html>
 <html>
+
 <head>
-	<title>Whomi</title>
+    <meta charset="UTF-8">
+    <title>Flat Login Form</title>
+    <link rel='stylesheet prefetch' href='http://daneden.github.io/animate.css/animate.min.css'>
+    <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,400italic,700italic,700'>
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Chelsea+Market' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
 </head>
+
 <body>
-
-<?php 
-	
-	if (isset($_POST['name'])){
-		echo "Hello, ".$_POST['name'];
-	}
-?>
-
-
+    <h1 class="center">Hello, [your name]!</h1>
+    <div class='form animated bounceIn'>
+        <h2>Hello, <?php echo $_POST["name"]; ?>
+         <br>
+         Have a nice day!
+         </h2>
+    </div>
+    <footer>
+        <h4>Made with &nbsp;<i class="fa fa-heart"></i>&nbsp;for Fossasia.org</footer>
 </body>
+
+
 </html>
